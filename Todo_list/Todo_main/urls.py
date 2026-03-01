b4cadd6bcf10c33bed7e3c11114f4781250f02e0
Rajesh <rajeshpar2005@gmail.com>
2026-03-01 15:37:23 +0530

    we made registrstion and login system

--- Todo_list/Todo_main/urls.py
+++ Todo_list/Todo_main/urls.py
@@ -20,7 +20,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
+    path('register/', views.register, name='register'),
+    path('', views.login_view, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
