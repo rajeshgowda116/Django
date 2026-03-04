4e144306750e2475af8efd252b5aba285213709a
Rajesh <rajeshpar2005@gmail.com>
2026-03-04 20:30:01 +0530

    We added login system

--- ID_Card_GEnerator/IdCard/urls.py
+++ ID_Card_GEnerator/IdCard/urls.py
@@ -20,6 +20,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.register, name='register'),
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
 ]
--- ID_Card_GEnerator/IdCard/views.py
+++ ID_Card_GEnerator/IdCard/views.py
@@ -1,16 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth  
 
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return render(request, 'login.html')
+            return redirect('home')
+        
     else:
         form = RegistrationForm()
         
     return render(request, 'register.html',{'form':form})
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        form=AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username=form.cleaned_data.get('username')
+            password=form.cleaned_data.get('password')
+            user=auth.authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+            return redirect('home')
+    else:
+        form=AuthenticationForm()
+
+    return render(request, 'login.html', {'form':form})
+
+def home(request):
+    
+    return render(request, 'home.html')
